1faf44d1bd9af8b3fdc9fbdf26875c91da30b7a0
alonebehappy <39368859+alonebehappy@users.noreply.github.com>
2019-12-10 19:50:07 +0800

    Create MyHttpClientUtils.java

---
+++ src/com/alibaba/util/MyHttpClientUtils.java
@@ -0,0 +1,192 @@
+package com.alibaba.util;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.alibaba.fastjson.JSONObject;
+
+/**
+ * HttpClient工具类
+ * 
+ */
+public class MyHttpClientUtils {
+	private static Logger logger = LoggerFactory.getLogger(MyHttpClientUtils.class); // 日志记录
+
+	private static RequestConfig requestConfig = null;
+
+	static {
+		// 设置请求和传输超时时间
+		requestConfig = RequestConfig.custom().setSocketTimeout(60000).setConnectTimeout(60000).build();
+	}
+
+	/**
+	 * post请求传输json参数
+	 * 
+	 * @param url
+	 * @param jsonParam
+	 * @return
+	 */
+	public static JSONObject httpPost(String url, JSONObject jsonParam) {
+		return httpPost(url, null, jsonParam);
+	}
+
+	/**
+	 * post请求传输json参数
+	 * 
+	 * @param url
+	 * @param headerMap
+	 * @param jsonParam
+	 * @return
+	 */
+	public static JSONObject httpPost(String url, Map<String, String> headerMap, JSONObject jsonParam) {
+		// post请求返回结果
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+		JSONObject jsonResult = null;
+		HttpPost httpPost = new HttpPost(url);
+		// 设置请求和传输超时时间
+		httpPost.setConfig(requestConfig);
+		try {
+			if (headerMap != null) {
+				for (Entry<String, String> entry : headerMap.entrySet()) {
+					httpPost.addHeader(entry.getKey(), entry.getValue());
+				}
+			}
+			if (null != jsonParam) {
+				// 解决中文乱码问题
+				StringEntity entity = new StringEntity(jsonParam.toString(), "utf-8");
+				entity.setContentEncoding("UTF-8");
+				entity.setContentType("application/json");
+				httpPost.setEntity(entity);
+			}
+			CloseableHttpResponse result = httpClient.execute(httpPost);
+			// 请求发送成功，并得到响应
+			if (result.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+				String str = "";
+				try {
+					// 读取服务器返回过来的json字符串数据
+					str = EntityUtils.toString(result.getEntity(), "utf-8");
+					// 把json字符串转换成json对象
+					jsonResult = JSONObject.parseObject(str);
+				} catch (Exception e) {
+					logger.error("post请求提交失败:" + url, e);
+				}
+			}
+		} catch (IOException e) {
+			logger.error("post请求提交失败:" + url, e);
+		} finally {
+			httpPost.releaseConnection();
+		}
+		return jsonResult;
+	}
+
+	/**
+	 * post请求传输String参数 例如：name=Jack&sex=1&type=2
+	 * Content-type:application/x-www-form-urlencoded
+	 * 
+	 * @param url
+	 *            url地址
+	 * @param strParam
+	 *            参数
+	 * @return
+	 */
+	public static JSONObject httpPostForm(String url, String strParam) {
+		// post请求返回结果
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+		JSONObject jsonResult = null;
+		HttpPost httpPost = new HttpPost(url);
+		httpPost.setConfig(requestConfig);
+		try {
+			if (null != strParam) {
+				// 解决中文乱码问题
+				StringEntity entity = new StringEntity(strParam, "utf-8");
+				entity.setContentEncoding("UTF-8");
+				entity.setContentType("application/x-www-form-urlencoded");
+				httpPost.setEntity(entity);
+			}
+			CloseableHttpResponse result = httpClient.execute(httpPost);
+			// 请求发送成功，并得到响应
+			if (result.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+				String str = "";
+				try {
+					// 读取服务器返回过来的json字符串数据
+					str = EntityUtils.toString(result.getEntity(), "utf-8");
+					// 把json字符串转换成json对象
+					jsonResult = JSONObject.parseObject(str);
+				} catch (Exception e) {
+					logger.error("post请求提交失败:" + url, e);
+				}
+			}
+		} catch (IOException e) {
+			logger.error("post请求提交失败:" + url, e);
+		} finally {
+			httpPost.releaseConnection();
+		}
+		return jsonResult;
+	}
+
+	/**
+	 * 发送get请求
+	 * 
+	 * @param url
+	 * @return
+	 */
+	public static JSONObject httpGet(String url) {
+		return httpGet(url, null);
+	}
+
+	/**
+	 * 发送get请求
+	 * 
+	 * @param url
+	 * @param headerMap
+	 * @return
+	 */
+	public static JSONObject httpGet(String url, Map<String, String> headerMap) {
+		// get请求返回结果
+		JSONObject jsonResult = null;
+		CloseableHttpClient client = HttpClients.createDefault();
+		// 发送get请求
+		HttpGet request = new HttpGet(url);
+		request.setConfig(requestConfig);
+		try {
+			CloseableHttpResponse response = client.execute(request);
+
+			if (headerMap != null) {
+				for (Entry<String, String> entry : headerMap.entrySet()) {
+					request.addHeader(entry.getKey(), entry.getValue());
+				}
+			}
+
+			// 请求发送成功，并得到响应
+			if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+				// 读取服务器返回过来的json字符串数据
+				HttpEntity entity = response.getEntity();
+				String strResult = EntityUtils.toString(entity, "utf-8");
+				// 把json字符串转换成json对象
+				jsonResult = JSONObject.parseObject(strResult);
+			} else {
+				logger.error("get请求提交失败:" + url);
+			}
+		} catch (IOException e) {
+			logger.error("get请求提交失败:" + url, e);
+		} finally {
+			request.releaseConnection();
+		}
+		return jsonResult;
+	}
+
+}
